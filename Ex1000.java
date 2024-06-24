4266df7a139396b3317baac1d82887a72d4b945e
joao <devjoaoroberto@gmail.com>
2024-06-24 17:29:17 -0300

    #1000

---
+++ Ex1000.java
@@ -0,0 +1,14 @@
+// beecrowd | 1000
+// Hello World!
+
+import java.io.IOException;
+
+public class Ex1000 {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}

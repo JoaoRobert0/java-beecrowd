190c65ae08eea6a5dfda781d3e54c565a9b0c4a8
joao <devjoaoroberto@gmail.com>
2024-06-24 17:31:07 -0300

    #1003

---
+++ Ex1003.java
@@ -0,0 +1,21 @@
+// beecrowd | 1003
+// Simple Sum
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ex1003 {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner scanner = new Scanner(System.in);
+        
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println("SOMA = " + (x + y));
+
+        scanner.close();
+
+    }
+ 
+}
